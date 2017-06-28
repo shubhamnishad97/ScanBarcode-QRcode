f0a83b4cd4cf2c2e3f35e267bc22572aa05285ec
Shubham Nishad <shubhamnishad25@gmail.com>
2017-06-28 13:25:10 +0530

    added the zxing library and attached it to fab

--- app/src/main/java/com/shubhamnishad/scanbarcodeqrcode/MainActivity.java
+++ app/src/main/java/com/shubhamnishad/scanbarcodeqrcode/MainActivity.java
@@ -1,29 +1,37 @@
 package com.shubhamnishad.scanbarcodeqrcode;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.util.Linkify;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
 
 public class MainActivity extends AppCompatActivity {
-
+    IntentIntegrator scan;
+    TextView tvOutput;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
+        tvOutput = (TextView)findViewById(R.id.tvOutput);
+        scan = new IntentIntegrator(this);
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                scan.initiateScan();
             }
         });
     }
@@ -48,5 +56,21 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+        if (result != null) {
+
+            if (result.getContents() == null) {
+                Toast.makeText(this, "Result Not Found", Toast.LENGTH_LONG).show();
+            } else {
+
+                tvOutput.setText(result.toString());
+                Linkify.addLinks(tvOutput, Linkify.WEB_URLS);
+
+            }
+        } else {
+            super.onActivityResult(requestCode, resultCode, data);
+        }
     }
 }
